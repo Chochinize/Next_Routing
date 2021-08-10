54e1e2d1af212457dcfee88b07f6f1e05e2acdd9
chochiniz <chochinize@gmail.com>
2021-08-10 15:55:41 +0200

    initial

---
+++ Components/Navbar.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Image from 'next/image';
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+            <Image src='/me.jpg' width={128} height={77}/>
+            </div>
+               <Link href='/'><a>Home</a></Link>
+               <Link href='/about'><a>About</a></Link>
+               <Link href='/martinpage'><a>Home</a></Link>
+        </nav>
+    );
+}
+ 
+export default Navbar;
---
+++ pages/more.js
@@ -0,0 +1,12 @@
+const More = () => {
+    
+    
+    
+    return (
+        <div>
+            <h2>ANOTHER DUMMY CONTENT</h2>
+        </div>
+      );
+}
+ 
+export default More;
